e93faa05fb24f0e98d03d6286fb98975209d34f6
TERRELL FORD <ford.terrell@magnoliaisd.local>
2017-02-13 08:41:15 -0600

    ugh, another message

--- project/src/main/Main.java
+++ project/src/main/Main.java
@@ -1,23 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
-import javax.swing.JOptionPane;
+import dummydata.StudentMaker;
+import java.io.IOException;
 
 /**
  *
- * @author ford.terrell
+ *
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "This program runs.");
+    public static void main(String[] args) throws IOException {
+        StudentMaker.createDummyStudents();
     }
+    
     
 }
