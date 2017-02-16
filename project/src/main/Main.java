a496d6f8f1efc408c3ba966353e4c223f69ff293
TERRELL FORD <ford.terrell@magnoliaisd.local>
2017-02-16 09:16:21 -0600

    added a course reader

--- project/src/main/Main.java
+++ project/src/main/Main.java
@@ -1,8 +1,7 @@
 package main;
 
-import io.Endorsement;
 import io.Schedule;
-import util.StudentMaker;
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -12,8 +11,9 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-      //  StudentMaker.createDummyStudents();
+        //  StudentMaker.createDummyStudents();
 //      Schedule.setNumberOfPeriods(7);
-      Schedule s = new Schedule(1);
+        util.ClassReader.reparseClasses(new File("classes"));
+        Schedule s = new Schedule(1);
     }
 }
