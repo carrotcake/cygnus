8697098d69489219a0484a7e2d40c259805dff62
carrotcake <johnson.benjamin@magnoliaisd.local>
2017-02-16 08:37:43 -0600

    added raw course info file

--- project/src/io/Course.java
+++ project/src/io/Course.java
@@ -13,12 +13,14 @@ public class Course {
     private final String code;
     private ArrayList<String> prereq;
     public static CourseList<Course> courses = new CourseList<>(); 
+    public final int length;
     
     @SuppressWarnings("LeakingThisInConstructor") //dont tell me what to do bud
-    public Course(String c, ArrayList<String> prq){
+    public Course(String c, ArrayList<String> prq, int l){
         code = c;
         prereq = prq;
         int index = courses.indexOfCode(c);
+        length = l;
         courses.add(index,this); //this is ok
     }
     
